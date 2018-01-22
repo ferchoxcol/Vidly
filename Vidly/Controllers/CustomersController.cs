5b33fa2e4102fd26f0b680605c0a7e2dac927ab3
Fernando.Portatil <hardbreaker18@gmail.com>
2018-01-22 00:08:35 -0500

    consultas a la base de datos.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,17 +11,29 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context=new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
             
-            return View(GetCustomers());
+            return View(_context.Customers.Include(c => c.MembershipType).ToList());
         }
 
         public ActionResult Details(int id)
         {
 
-            Customer customer = GetCustomers().SingleOrDefault(c=> c.Id == id);
+            Customer customer = _context.Customers.SingleOrDefault(c=> c.Id == id);
 
             if (customer== null)
             {
