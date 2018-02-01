18abd5bebd87c9d96e96a413660f71b143b668ec
Fernando.Trabajo <hardbreaker18@gmail.com>
2018-02-01 09:16:49 -0500

    Sesión 4. Creating and updating Customer.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -53,21 +53,55 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes  =membershipTypes
             };
 
 
-            return View(viewModel);
+            return View("CustomerForm",viewModel);
         }
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
+            if (customer.Id==0)
+            {
             _context.Customers.Add(customer);
-            _context.SaveChanges();
+            
+            }
+            else
+            {
+              
+                var customerInDB = _context.Customers.Single(c => c.Id == customer.Id);
+              
+                customerInDB.Name = customer.Name;
+                customerInDB.BirthDate = customer.BirthDate;
+                customerInDB.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+                customerInDB.MembershipTypeId = customer.MembershipTypeId;
+            }
+
+            _context.SaveChanges(); 
            return  RedirectToAction("Index", "Customers");
         }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer== null)
+            return  HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer
+                ,MembershipTypes = _context.MembershipTypes.ToList()
+
+            };
+            return View("CustomerForm",viewModel);
+        }
+
+        
+
     }
 
 
